94d2f31dac02afb3eea0c30e5df4619b444450bb
RoyJake <royjake629@gmail.com>
2024-11-25 21:14:09 +0000

    completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,27 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+
+import "./page.css";
+import benz from "../public/benz.jpg";
+import challenger from "../public/challenger.jpg";
+import cybertruck from "../public/cybertruck.jpg";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main>
+      <div className="slider-container">
+        <div className="slide">
+          <Image width={5225} height={6531} alt="" src={benz.src} quality={50} placeholder="empty" />
+          <a href=''>Mercedes</a>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="slide">
+          <Image width={4000} height={6000} alt='' src={cybertruck.src} quality={50} placeholder="empty"/>
+          <a href=''>Cybertruch</a>
+        </div>
+        <div className="slide">
+          <Image width={3697} height={5546} alt='' src={challenger.src} quality={50} placeholder="empty"/>
+          <a href=''>Dodge</a>
+        </div>
+      </div>
+    </main>
   );
 }
